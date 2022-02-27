d271052f09f38250d52a19b435dc595cff737dfb
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2022-02-26 16:08:12 -0800

    : Home page implementation
    Components :
    - Announcements.jsx
    - Navbar.jsx
    - Slider.jsx
    - Categories.jsx
    - CategoryItem.jsx
    
    Pages :
    - Home,jsx
    - App.jsx
    
    src :
    - Data.js
    
    Created:
    - Announcement bar to highlight deals on top of page.
    - Create Navbar with Language selection, searchbox, Title, Register, Signin and Cart
    - Create Slider in page to highlight sales and show pictures.
    - Create Categories at bottom of home page with buttons for shop now

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const sliderItems = [
+	{
+		id: 1,
+		img: "https://images.unsplash.com/photo-1550614000-4895a10e1bfd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+		title: "SPRING SALE 45% OFF",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		bg: "fbf0f4",
+	},
+	{
+		id: 2,
+		img: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1440&q=80",
+		title: "SUMMER COLLECTION",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		bg: "f4edd2",
+	},
+	{
+		id: 3,
+		img: "https://images.unsplash.com/photo-1539109136881-3be0616acf4b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+		title: "WINTER COLLECTION",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		bg: "fcf1ed",
+	},
+];
+
+export const categories = [
+	{
+		id: 1,
+		img: "https://images.pexels.com/photos/1024036/pexels-photo-1024036.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		title: "SUMMER COLLECTION",
+	},
+	{
+		id: 2,
+		img: "https://images.pexels.com/photos/1734202/pexels-photo-1734202.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+		title: "WINTER COLLECTION",
+	},
+	{
+		id: 3,
+		img: "https://images.pexels.com/photos/1392154/pexels-photo-1392154.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		title: "SWIM COLLECTION",
+	},
+];
