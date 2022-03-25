7490cb9c2106019b74837d3c1c860464108f8f88
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2022-03-24 19:15:06 -0700

    Get token from local storage

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5001/api/";
-const TOKEN =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMzE0ZDZlNzBkOGUyOWUzNDczODk0OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0ODE0MzkwMSwiZXhwIjoxNjQ4NDAzMTAxfQ.kDOT5Zf5INDG0ZfdUaX7hVsZOeokfWX8pO5p_s6sEsk";
+
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
 	baseURL: BASE_URL,
 });
+
 export const userRequest = axios.create({
 	baseURL: BASE_URL,
 	header: { token: `Bearer ${TOKEN}` },
